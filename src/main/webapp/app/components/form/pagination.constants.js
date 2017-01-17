bdb6885dd00f8ec82f653800c2c77fcf417bfdb2
Naresh Miriyala <naresh.miriyala@jemena.com>
2017-01-17 13:03:33 +1100

    First Commit

---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('angularSpringGulpApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
